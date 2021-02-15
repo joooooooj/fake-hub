61b47fe70fd876da9c65169e836d5ea83c2019bb
ivanabrkic <ivanabrkic192@gmail.com>
2021-02-15 18:44:34 +0100

    Changed path

--- FakeHub/settings.py
+++ FakeHub/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'FakeHub.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'build/static')],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
