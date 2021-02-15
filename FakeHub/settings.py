4316505c66b16b606e67d856dbdd6445ee90ad34
ivanabrkic <ivanabrkic192@gmail.com>
2021-02-15 18:30:28 +0100

    Changed middleware

--- FakeHub/settings.py
+++ FakeHub/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.django.DjangoWhiteNoise'
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'FakeHub.urls'
