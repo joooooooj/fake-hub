3edc4d67caa69de6d201b7098c74e82fd598d40b
ivanabrkic <ivanabrkic192@gmail.com>
2021-02-15 20:33:05 +0100

    Changed routes

--- fakehub_app/urls.py
+++ fakehub_app/urls.py
@@ -1,28 +1,22 @@
 # backend/urls.py
 
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from rest_framework import routers
 from .views import TeamViewSet, UserViewSet, RepositoryViewSet, ProjectViewSet, LabelViewSet, MilestoneViewSet, \
     BranchViewSet, CommitViewSet
 
-# router = routers.SimpleRouter()
-# router.register(r'team', TeamViewSet)
-# router.register(r'user', UserViewSet)
-# router.register(r'repository', RepositoryViewSet)
-# router.register(r'project', ProjectViewSet)
-# router.register(r'label', LabelViewSet)
-# router.register(r'milestone', MilestoneViewSet)
-# router.register(r'branch', BranchViewSet)
-# router.register(r'commit', CommitViewSet)
+router = routers.SimpleRouter()
+router.register(r'team', TeamViewSet)
+router.register(r'user', UserViewSet)
+router.register(r'repository', RepositoryViewSet)
+router.register(r'project', ProjectViewSet)
+router.register(r'label', LabelViewSet)
+router.register(r'milestone', MilestoneViewSet)
+router.register(r'branch', BranchViewSet)
+router.register(r'commit', CommitViewSet)
 
 urlpatterns = [
-    re_path(r'team', TeamViewSet.as_view()),
-    re_path(r'user', UserViewSet.as_view()),
-    re_path(r'repository', RepositoryViewSet.as_view()),
-    re_path(r'project', ProjectViewSet.as_view()),
-    re_path(r'label', LabelViewSet.as_view()),
-    re_path(r'milestone', MilestoneViewSet.as_view()),
-    re_path(r'branch', BranchViewSet.as_view()),
-    re_path(r'commit', CommitViewSet.as_view())
+    path('', include(router.urls)),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
