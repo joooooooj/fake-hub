1cdc6e362d90a0a55e4d84db077f93fa5a43cbff
ivanabrkic <ivanabrkic192@gmail.com>
2021-02-15 18:11:21 +0100

    Changed db cred

--- FakeHub/settings.py
+++ FakeHub/settings.py
@@ -83,10 +83,10 @@ WSGI_APPLICATION = 'FakeHub.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd6mbqkfq3ffmfm', #fake-hub
-        'USER': 'ckonhstkfzyabt', #admin
-        'PASSWORD': 'a57a05fb5b600c7656929dc8aec94429b95374482cbe3fac368d2e6c95a8cfe2', #admin
-        'HOST': 'ec2-54-247-158-179.eu-west-1.compute.amazonaws.com', #change to localhost when testing on localhost, and postgres_db when dockerizing
+        'NAME': 'd8ttnndbim4t8o', #fake-hub
+        'USER': 'blninykuhbotdv', #admin
+        'PASSWORD': '7139c26ca614f959eae973310e90bc488a6614dbde986e3c96d51fde8ac06308', #admin
+        'HOST': 'ec2-54-216-185-51.eu-west-1.compute.amazonaws.com', #change to localhost when testing on localhost, and postgres_db when dockerizing
         'PORT': 5432,
         'DISABLE_SERVER_SIDE_CURSORS': True
     }
